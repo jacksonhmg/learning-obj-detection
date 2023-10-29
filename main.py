44259de256f784c482d5184dd328fcece3660550
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-29 14:45:03 -0700

    setup video capture

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+from ultralytics import YOLO
+
+cap = cv2.VideoCapture("dogs.mp4")
+
+while True:
+    ret, frame = cap.read()
+    if not ret: 
+        break
+
+    cv2.imshow("Img", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
