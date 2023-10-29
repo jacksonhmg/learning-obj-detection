68dc2b7e72c1076770c7f4f834ade3ddec6d2204
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-29 14:54:10 -0700

    drawing rect on objects

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import cv2
 from ultralytics import YOLO
+import numpy as np
 
 cap = cv2.VideoCapture("dogs.mp4")
+
+model = YOLO("yolov8m.pt")
+
 
 while True:
     ret, frame = cap.read()
     if not ret: 
         break
 
+    results = model(frame)
+    result = results[0]
+    bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
+    for bbox in bboxes:
+        x1, y1, x2, y2 = bbox
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+
     cv2.imshow("Img", frame)
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(0)
     if key == 27:
         break
 
